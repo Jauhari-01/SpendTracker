20db99de154ee100db6883cbb0072984e1496235
Shubham_Jauhari <shubham.jauhari009@gmail.com>
2022-05-10 00:43:59 +0530

    Added some functionalities

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NativeBaseProvider } from 'native-base';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -16,25 +17,19 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
+    <NativeBaseProvider>
+      <Provider store={store}>
+        <NavigationContainer>
 
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Spend Tracker' }} />
-          <Stack.Screen name="Add" component={AddTransaction} options={{ title: 'Add Expense' }} />
-        </Stack.Navigator>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Spend Tracker' }} />
+            <Stack.Screen name="Add" component={AddTransaction} options={{ title: 'Add Expense' }} />
+          </Stack.Navigator>
 
-      </NavigationContainer>
-    </Provider>
+        </NavigationContainer>
+      </Provider>
+    </NativeBaseProvider>
   );
 }
 
 export default App;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/Component/HomeScreen.js
+++ src/Component/HomeScreen.js
@@ -1,36 +1,73 @@
 
 import * as React from 'react';
-import { View, Text ,StyleSheet,FlatList} from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 // import LinearGradient from 'react-native-linear-gradient';
-import {LinearGradient} from 'expo-linear-gradient';
-import {Button,Container,CheckBox,Body,Right} from 'native-base';
+import { LinearGradient } from 'expo-linear-gradient';
+import { NativeBaseProvider, Button, Container, CheckBox, Body, Right } from 'native-base';
 import Animated from 'react-native-reanimated';
 
 
 
-export default function HomeScreen() {
+export default function HomeScreen({navigation}) {
   return (
-    <Animated.View style={{ 
-      flex: 1, 
-      alignItems: 'center',
-      paddingVertical: 10,
-      paddingHorizantal: 20 }}>
-      <LinearGradient 
-        colors={['#FAAD3D','#EFC90A','#F1CB0C']}
-        style={styles.Box}
-      >
-        <View></View>
-      </LinearGradient>
-    </Animated.View>
+    <Container>
+      <Animated.View style={{
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        paddingVertical: 20,
+        paddingHorizantal: 20
+      }}>
+        <LinearGradient
+          colors={['#FAAD3D', '#EFC90A', '#F1CB0C']}
+          style={{ ...styles.Box }}
+        >
+          <View style={{
+            width: '70%',
+            alignItems: 'flex-start'
+          }}>
+            <Text style={{
+              fontSize: 15,
+              color: '#fff',
+              fontWeight: '600'
+            }}>Current Balance</Text>
+            <Text style={{
+              fontSize: 32,
+              color: '#fff',
+              fontWeight: '700'
+            }}>
+              ₹10,000</Text>
+          </View>
+          <View>
+            <Button
+              // rounded
+              light
+              style={{
+                padding: 10,
+                marginTop: 32,
+                borderWidth:3,
+                borderColor:'#fff',
+                backgroundColor: '#E10C62',
+                alignItem: 'center',
+                justifyContent: 'center'
+              }}
+              onPress={() => navigation.navigate('Add')}
+            >
+              <Text style={{ color: '#fff', fontWeight: '700' }}>Add</Text>
+            </Button>
+          </View>
+        </LinearGradient>
+      </Animated.View>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
-  Box:{
-    width:'100%',
+  Box: {
+    width: '100%',
     height: 189,
-    borderRadius:15,
-    flexDirection:'row',
-    padding:22
+    borderRadius: 15,
+    flexDirection: 'row',
+    padding: 22
   }
 })
