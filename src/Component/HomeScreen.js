816561b0c674e10fa8813d74be646eef9a13df6e
Shubham_Jauhari <shubham.jauhari009@gmail.com>
2022-05-09 23:23:44 +0530

    Added expo-liner-gradient instead of react-native-linear-gradient --- and --- added some dependecies --- safe state 02

--- src/Component/HomeScreen.js
+++ src/Component/HomeScreen.js
@@ -1,13 +1,36 @@
 
 import * as React from 'react';
 import { View, Text ,StyleSheet,FlatList} from 'react-native';
+// import LinearGradient from 'react-native-linear-gradient';
+import {LinearGradient} from 'expo-linear-gradient';
+import {Button,Container,CheckBox,Body,Right} from 'native-base';
+import Animated from 'react-native-reanimated';
+
 
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center',backgroundColor:'red' }}>
-      <Text>Home Screen</Text>
-    </View>
+    <Animated.View style={{ 
+      flex: 1, 
+      alignItems: 'center',
+      paddingVertical: 10,
+      paddingHorizantal: 20 }}>
+      <LinearGradient 
+        colors={['#FAAD3D','#EFC90A','#F1CB0C']}
+        style={styles.Box}
+      >
+        <View></View>
+      </LinearGradient>
+    </Animated.View>
   );
 }
 
+const styles = StyleSheet.create({
+  Box:{
+    width:'100%',
+    height: 189,
+    borderRadius:15,
+    flexDirection:'row',
+    padding:22
+  }
+})
