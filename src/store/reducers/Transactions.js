e85ff5792f51a96f4f6c36ff5d27615891425d57
Shubham_Jauhari <shubham.jauhari009@gmail.com>
2022-05-22 21:29:47 +0530

    Added minner changes and added some styling and comments

--- src/store/reducers/Transactions.js
+++ src/store/reducers/Transactions.js
@@ -2,11 +2,11 @@ import {ADD_TRANSACTION,DELETE_TRANSACTION} from '../actions/types';
 
 const initialstate = {
     transactions : [
-        {id:1,title:'Soup1',price:-22},
-        {id:2,title:'Soup2',price:400},
-        {id:3,title:'Soup3',price:189},
-        {id:4,title:'Soup4',price:122},
-        {id:5,title:'Soup5',price:986},
+        // {id:1,title:'Soup1',price:-22},
+        // {id:2,title:'Soup2',price:400},
+        // {id:3,title:'Soup3',price:189},
+        // {id:4,title:'Soup4',price:122},
+        // {id:5,title:'Soup5',price:986},
     ],
 };
 
