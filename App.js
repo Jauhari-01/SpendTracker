711100094fab347c65faa467c4ce98af61739e05
Shubham_Jauhari <shubham.jauhari009@gmail.com>
2022-05-22 21:27:38 +0530

    Added changes for in navigation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
@@ -22,8 +22,8 @@ function App() {
         <NavigationContainer>
 
           <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Spend Tracker' }} />
-            <Stack.Screen name="Add" component={AddTransaction} options={{ title: 'Add Expense' }} />
+            <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'SPEND TRACKER' }} />
+            <Stack.Screen name="Add" component={AddTransaction} options={{ title: 'ADD EXPENSEAdd Expense' }} />
           </Stack.Navigator>
 
         </NavigationContainer>
